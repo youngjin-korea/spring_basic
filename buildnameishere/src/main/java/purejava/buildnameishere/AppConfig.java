877ff577c2c31e8357d5d4586791b5d5b28bec13
YoungJinKim <youngjin1293@naver.com>
2024-08-23 17:49:01 +0900

    [feat] 메서드명: 역할, return 값 구현체를 한눈에 파악하도록 리팩토링

--- buildnameishere/src/main/java/purejava/buildnameishere/AppConfig.java
+++ buildnameishere/src/main/java/purejava/buildnameishere/AppConfig.java
@@ -1,6 +1,7 @@
 package purejava.buildnameishere;
 
 
+import purejava.buildnameishere.discount.DiscountPolicy;
 import purejava.buildnameishere.discount.FixDiscountPolicy;
 import purejava.buildnameishere.member.MemberService;
 import purejava.buildnameishere.member.MemberServiceImpl;
@@ -8,13 +9,22 @@ import purejava.buildnameishere.member.MemoryMemberRepository;
 import purejava.buildnameishere.order.OrderService;
 import purejava.buildnameishere.order.OrderServiceImpl;
 
+// 메서드명: 역할, return 값 구현체를 한눈에 파악하도록 리팩토링
 public class AppConfig {
     public MemberService memberService() {
         // 생성자 주입 - memberserviceImpl의 DIP 규칙을 만족시켜줌
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
